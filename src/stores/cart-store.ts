327adc011088f2fbae713de3835211617cc19680
AB1738 <saintsring2017@yahoo.com>
2025-04-23 17:13:45 -0400

    added cart persistance in local storage using zustand

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,6 +1,8 @@
 
 
 import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
+
 
 export type CartItem={
     id:number,
@@ -20,62 +22,70 @@ export type CartStore = {
   removeFromCart:(id:number)=>void,
   clearCart:()=>void
 }
-export const useCartStore = create<CartStore>((set) => ({
-    cart:[],
-    totalItems:0,
-    totalPrice:0,
-    addToCart:(item)=>set((state)=>{
-        const foundProduct=state.cart.find((prod)=>prod.id===item.id)
 
 
 
-        if(foundProduct){
+export const useCartStore = create<CartStore>()(
+    persist(
+        (set) => ({
+            cart:[],
+            addToCart:(item)=>set((state)=>{
+                const foundProduct=state.cart.find((prod)=>prod.id===item.id)
+        
+        
+        
+                if(foundProduct){
+        
+               return {cart:state.cart.map((prduct)=>{
+                    if(foundProduct.id===prduct.id){
+                        return {...prduct,qty:prduct.qty+1}
+                    }else
+                    return prduct
+                })}
+        
+                }
+                else{
+                    return {cart:[...state.cart,item]}
+                }
+        }),
+        incrementQty:(id)=>set((state)=>{
+            const foundProduct=state.cart.find((prod)=>prod.id===id)
+            if(foundProduct){
+        
+                return {cart:state.cart.map((prduct)=>{
+                     if(foundProduct.id===prduct.id){
+                         return {...prduct,qty:prduct.qty+1}
+                     }else
+                     return prduct
+                 })}
+         
+                 }
+                 return { cart: state.cart };
+        
+        }),
+        decrementQty:(id)=>set((state)=>{
+            const foundProduct=state.cart.find((prod)=>prod.id===id)
+            if(foundProduct){
+        
+                return {cart:state.cart.map((prduct)=>{
+                     if(foundProduct.id===prduct.id){
+                         return {...prduct,qty:prduct.qty-1}
+                     }else
+                     return prduct
+                 }).filter((prod)=>prod.qty!==0)}
+         
+                 }
+                 return { cart: state.cart };
+        
+        }),
+            removeFromCart:(id)=>set((state)=>({cart:state.cart.filter((prod)=>prod.id!==id)})),
+        
+            clearCart:()=>set({cart:[]})
+          }),       {
+                     name: 'cart-storage',
+                   storage: createJSONStorage(() => localStorage),
+                 },
+    )
+)
 
-       return {cart:state.cart.map((prduct)=>{
-            if(foundProduct.id===prduct.id){
-                return {...prduct,qty:prduct.qty+1}
-            }else
-            return prduct
-        })}
-
-        }
-        else{
-            return {cart:[...state.cart,item]}
-        }
-}),
-incrementQty:(id)=>set((state)=>{
-    const foundProduct=state.cart.find((prod)=>prod.id===id)
-    if(foundProduct){
-
-        return {cart:state.cart.map((prduct)=>{
-             if(foundProduct.id===prduct.id){
-                 return {...prduct,qty:prduct.qty+1}
-             }else
-             return prduct
-         })}
- 
-         }
-         return { cart: state.cart };
-
-}),
-decrementQty:(id)=>set((state)=>{
-    const foundProduct=state.cart.find((prod)=>prod.id===id)
-    if(foundProduct){
-
-        return {cart:state.cart.map((prduct)=>{
-             if(foundProduct.id===prduct.id){
-                 return {...prduct,qty:prduct.qty-1}
-             }else
-             return prduct
-         }).filter((prod)=>prod.qty!==0)}
- 
-         }
-         return { cart: state.cart };
-
-}),
-    removeFromCart:(id)=>set((state)=>({cart:state.cart.filter((prod)=>prod.id!==id)})),
-
-    clearCart:()=>set({cart:[]})
-  }));
-  
 
